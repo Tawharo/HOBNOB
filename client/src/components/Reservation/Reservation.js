37d66db47ec8e7962034326949ca33c71a8335d4
Anthony Osiol <funnynoun@gmail.com>
2020-07-11 14:06:36 -0400

    test

--- client/src/components/Reservation/Reservation.js
+++ client/src/components/Reservation/Reservation.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 
 function Reservation() {
@@ -62,39 +61,6 @@ function Reservation() {
       <button>Book Now RESY</button>
     </>
   );
-=======
-import React from 'react';
-
-
-function Reservation() {
-    return (
-        <>
-            <h1>HOBNOB</h1>
-            <p>eat. drink. socialize</p>
-            <p>PLEASE NOTE: With the rapidly changing nature of events related to COVID-19, we can not anticipate how long our patio and dining room will remain closed. WE ARE CURRENTLY NOT ACCEPTING RESERVATIONS.</p>
-            <p>We are a small restaurant with limited seating. Click the red “Book Now RESY” button below to make a reservation.</p>
-            <ul>Here’s the inside scoop:
-                <li>Dinner and Brunch reservations are highly recommended! Come early or late for the best chance to snag a walk-in table. </li>
-                <li>For lunch, we only take reservations for parties of 5 or more. Table turnover is fairly quick; we’ll do our best to get you in and out!</li>
-                <li>Bar is first come, first served. </li>
-            </ul>
-            <ul>A few more notes:
-                <li>There are several other independent Hobnob restaurants throughout North America; please make sure you are making a reservation in Richmond, Virginia, using the <strong>Resy</strong> app - click the button below. We do not use OpenTable.</li>
-                <li>Making a reservation for a special event? Call in advance about ordering a homemade pie or cake for your celebration; <strong>Parties bringing in outside desserts will be charged a $15 plating fee.</strong></li>
-                <li>If you have trouble making a reservation, give us a call at 804-264-7400 during normal business hours.</li>
-            </ul>
-            <ul>Parking
-                <li>Ample parking is available in our lot (including next to the dumpsters and on either side of the building) or along Kenwood Avenue. <strong>Parking across the street at Hermitage Grocery is prohibited; vehicles will be towed at the owner’s expense.</strong></li>
-            </ul>
-            <button>Book Now RESY</button>
-            <footer className="image-div">
-                <p>Powered by JATAG</p>
-                <img src="https://www.kindpng.com/picc/m/20-209935_facebook-icon-clipart-social-media-icons-grey-facebook.png" alt="fb" />
-                <img src="https://www.beerinstitute.org/wp-content/uploads/2017/07/instagram-icon-grey.3cf404e5.png" alt="insta" />
-            </footer>
-        </>
-    )
->>>>>>> 20921d83b1f262b6d895cde07c668e6bfb603761
 }
 
 export default Reservation;
