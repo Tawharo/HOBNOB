64ae270da4d2ccd25a4fdb3ed9419fb45a21cb2d
lets travel <george@brewsystems.com>
2020-07-19 09:44:42 -0400

    admin work

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -5,6 +5,51 @@ import { Link } from "react-router-dom";
 import { useAuth } from "../utils/auth";
 import { Button, Table } from "react-bootstrap";
 import Menu from "../components/Menu"
+import Logo from "../components/Logo";
+import MenuItem from "../components/MenuItem";
+
+// function Menu() {
+//   const [food, setFood] = useState([]);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       const result = await API.getMenu();
+//       setFood(result.data.Menu);
+//     };
+
+//     fetchData();
+//   }, []);
+
+//   return (
+//     <>
+//       <div className="container-menu">
+//         <Logo />
+
+//         {food &&
+//           food.length > 0 &&
+//           identifyMenuGroups(food).map((menuGroup) => (
+//             <>
+//               <h4 className="menu-group">{menuGroup}</h4>
+//               <br></br>
+//               <div className="menu-field">
+//                 {food
+//                   .filter((menuItem) => menuItem.type === menuGroup)
+//                   .map((menuItem) => (
+//                     <MenuItem props={menuItem} key={menuItem.menuItemName} />
+//                   ))}
+//               </div>
+//             </>
+//           ))}
+//       </div>
+//     </>
+//   );
+// }
+
+// function identifyMenuGroups(foodArray) {
+//   return Array.from(new Set(foodArray.map((x) => x.type)));
+// }
+
+// export default Menu;
 
 function Admin() {
   const [username, setUsername] = useState("");
