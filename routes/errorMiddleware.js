1c014540c84deec78eef6d162c299ed8082dc4f6
lets travel <george@brewsystems.com>
2020-07-20 13:05:14 -0400

    fixed jwt version issue

--- routes/errorMiddleware.js
+++ routes/errorMiddleware.js
@@ -1,6 +1,7 @@
 // Error handling middleware
 module.exports = (err, req, res, next) => {
   if (err.name === "UnauthorizedError") {
+    console.log(req);
     // Send the error rather than to show it on the console
     res.status(401).send(err);
   } else {
