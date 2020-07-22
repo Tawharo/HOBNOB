94864081f9924f10bc291c74ccb61104338bb608
Anthony Osiol <funnynoun@gmail.com>
2020-07-22 15:35:10 -0400

    added order page and removed buttons

--- client/src/components/MenuItem/index.js
+++ client/src/components/MenuItem/index.js
@@ -1,16 +1,35 @@
+// import React from "react";
+// import "./style.css";
+
+// function MenuItem(props) {
+//   return (
+//     <div className="menu-card">
+//       <div className="menu-item">
+//         <div className="menu-title">{props.props.menuItemName}</div>
+//         <span className="price">{props.props.price}</span>
+//         <div className="menu-description">{props.props.ingredients}</div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default MenuItem;
+
 import React from "react";
-import "./style.css";
+import { Card } from "react-bootstrap";
 
 function MenuItem(props) {
   return (
-    <div className="menu-card">
-      <div className="menu-item">
-        <div className="menu-title">{props.props.menuItemName}</div>
-        <span className="price">{props.props.price}</span>
-        <div className="menu-description">{props.props.ingredients}</div>
-      </div>
-    </div>
+    <Card style={{ width: "30rem" }}>
+      {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+      <Card.Body>
+        <Card.Title>
+          {" "}
+          {props.props.menuItemName} {props.props.price}
+        </Card.Title>
+        <Card.Text>{props.props.ingredients}</Card.Text>
+      </Card.Body>
+    </Card>
   );
 }
-
 export default MenuItem;
---
+++ client/src/pages/Order.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from "react";
+import API from "../utils/API";
+import Wrapper from "../components/Wrapper";
+import { Link } from "react-router-dom";
+import { useAuth } from "../utils/auth";
+import { Button, Container, Row, Col, Table } from "react-bootstrap";
+import Logo from "../components/Logo";
+import OrderItem from "../components/OrderItem";
+function Order() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [role, setRole] = useState("");
+  const { user } = useAuth();
+  const [menuItems, setMenuItems] = useState([]);
+  useEffect(() => {
+    API.getUser(user.id).then((res) => {
+      setUsername(res.data.username);
+      setEmail(res.data.email);
+      setRole(res.data.role);
+    });
+  }, [user]);
+  const getItemsFromApi = () => {
+    API.getMenu().then((res) => {
+      setMenuItems(res.data.Menu);
+      console.log(res.data.Menu);
+      console.log("testing testing");
+    });
+  };
+  return (
+    <div>
+      <Logo />
+      <Container>
+        <Row>
+          <Col>
+            <h3>Welcome {username}!</h3>
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <Row>
+          <Col>
+            <Button variant="primary" onClick={getItemsFromApi}>
+              Get Menu
+            </Button>
+            {""}
+          </Col>
+          <Col>
+            <Button variant="info" onClick={getItemsFromApi}>
+              Get Menu
+            </Button>
+            {""}
+          </Col>
+          <Col>
+            <Button variant="success" onClick={getItemsFromApi}>
+              Get Menu
+            </Button>
+            {""}
+          </Col>
+          <Col>
+            <Button variant="info" onClick={getItemsFromApi}>
+              Get Menu
+            </Button>
+            {""}
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <Table>
+          <Row>
+            <Col xs={6}>
+              {menuItems.map((menuItem) => (
+                <OrderItem props={menuItem} key={menuItem.menuItemName} />
+              ))}
+            </Col>
+          </Row>
+        </Table>
+      </Container>
+    </div>
+  );
+}
+export default Order;
