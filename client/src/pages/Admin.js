0cf804d287acd11da0c13daad585de5070f6d332
lets travel <george@brewsystems.com>
2020-07-23 16:04:49 -0400

    updated form and created dynamic form feild

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,20 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import API from "../utils/API";
 import { Link } from "react-router-dom";
 import { useAuth } from "../utils/auth";
-import { Button, Container, Row, Col, Table } from "react-bootstrap";
+import { Button, Container, Row, Col, Grid, Form } from "react-bootstrap";
 import Logo from "../components/Logo";
-import MenuItem from "../components/MenuItem";
+import OrderItem from "../components/OrderItem";
 function Admin() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [role, setRole] = useState("");
   const { user } = useAuth();
-  const [menuTable, setMenuTable] = useState("");
   const [showCreateItem, setShowCreateItem] = useState(false);
   const [showViewOrders, setShowViewOrders] = useState(false);
   const [showEditItem, setShowEditItem] = useState(false);
   const [menuItems, setMenuItems] = useState([]);
+  const [inputFields, setInputFields] = useState([{ Ingredient: ''}]);
+
   useEffect(() => {
     API.getUser(user.id).then((res) => {
       setUsername(res.data.username);
@@ -42,6 +43,38 @@ function Admin() {
       console.log("testing testing");
     });
   };
+    
+  
+    const handleAddFields = () => {
+      const values = [...inputFields];
+      values.push({ ingredient: ''});
+      setInputFields(values);
+    };
+  
+    const handleRemoveFields = index => {
+      const values = [...inputFields];
+      values.splice(index, 1);
+      setInputFields(values);
+    };
+  
+    const handleInputChange = (index, event) => {
+      const values = [...inputFields];
+      if (event.target.name === "ingredient") {
+        values[index].firstName = event.target.value;
+      } 
+  
+      setInputFields(values);
+    };
+  
+    const handleSubmit = e => {
+      e.preventDefault();
+      console.log("inputFields", inputFields);
+    };
+
+    const postNewMenuItem = () => {
+
+    };
+
   return (
     <div>
       <Logo />
@@ -75,13 +108,70 @@ function Admin() {
         </Row>
       </Container>
       <Container>
-        {showCreateItem && <div>show create menu item here.</div>}
+        {showCreateItem && (
+          <Form>
+          <Form.Row>
+            <Form.Group as={Col} controlId="formMenuItem">
+              <Form.Label>Menu Item</Form.Label>
+              <Form.Control type="menuItemName" placeholder="Menu Item Name" />
+            </Form.Group>
+        
+            <Form.Group as={Col} controlId="formMenuItemPrice">
+              <Form.Label>Price</Form.Label>
+              <Form.Control type="price" placeholder="Price" />
+            </Form.Group>
+          </Form.Row>
+          <form>
+        <div className="form-row">
+          {inputFields.map((inputField, index) => (
+            <Fragment key={`${inputField}~${index}`}>
+              <div className="form-group col-sm-8">
+                <label htmlFor="ingredient">Ingredient</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="ingredient"
+                  name="ingredient"
+                  value={inputField.ingredient}
+                  onChange={event => handleInputChange(index, event)}
+                />
+              </div>
+              
+              <div className="form-group col-sm-2">
+                <button
+                  className="btn btn-link"
+                  type="button"
+                  onClick={() => handleRemoveFields(index)}
+                >
+                  -
+                </button>
+                <button
+                  className="btn btn-link"
+                  type="button"
+                  onClick={() => handleAddFields()}
+                >
+                  +
+                </button>
+              </div>
+            </Fragment>
+          ))}
+        </div>
+       
+      </form>
+          <Form.Group id="formIsSpecial">
+            <Form.Check type="checkbox" label="I'm on special" />
+          </Form.Group>
+          <Button variant="primary" type="submit" onClick="postNewMenuItem">
+            Submit
+          </Button>
+        </Form>
+        )}
         {showEditItem && (
-          <Table>
+         
             <Row>
               <Col xs={6}>
                 {menuItems.map((menuItem) => (
-                  <MenuItem
+                  <OrderItem
                     props={menuItem}
                     key={menuItem.menuItemName}
                     isEdit={true}
@@ -89,7 +179,7 @@ function Admin() {
                 ))}
               </Col>
             </Row>
-          </Table>
+
         )}
         {showViewOrders && <div>show orders here.</div>}
       </Container>
